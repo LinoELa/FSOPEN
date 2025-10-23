37d2302176f92b2e3e9bd98065ca1186c2f3b116
paulino-omnitec <paulino.salvador@omnitecsystems.com>
2025-10-23 16:46:48 +0200

    Guardar para cambiar de Rama

--- PART-1/src/App.jsx
+++ PART-1/src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// ################# Componente App Simple #################
+export function AppSimple() {
+  console.log("Hello from AppSimple Component");
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log("APP Now : ", now, a + b);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>Bienvenido {now.toString()}</p>
+      <p>
+        {a} + {b} = {a + b}
       </p>
     </>
-  )
+  );
 }
 
-export default App
+// ################# Componente App Multiple #################
+function HelloMultiple() {
+  return (
+    <div>
+      <h2>Hello Component</h2>
+    </div>
+  );
+}
+
+export function AppMultiple() {
+  console.log("Hello from AppMultiple Component");
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log("APP Now : ", now, a + b);
+  return (
+    <main>
+      <h1>Componentes Múltiples</h1>
+      <p>Texto introductorio que es solo inline.</p>
+      <HelloMultiple />
+      <HelloMultiple />
+    </main>
+  );
+}
+
+// ################# Props: pasar datos a componentes #################
+function HelloProps({ name, age, time }) {
+  console.log("props en Hello: ", { name, age, time });
+  return (
+    <div>
+      <h2>
+        Hello {name}, you are {age} years old
+      </h2>
+    </div>
+  );
+}
+
+export function AppProps() {
+  console.log("Hello from AppProps Component");
+  const now = new Date();
+  const name = "Lino";
+  const age = 20;
+  return (
+    <main>
+      <h1>Props</h1>
+      <HelloProps name={name} age={age} time={now} />
+      <HelloProps name="Antonio" age={20 - 5} />
+    </main>
+  );
+}
+
+// ################# No renderizar Objetos #################
+export function AppNoRender() {
+  const noRenderizado = "Objeto No Renderizado";
+  const friends = [
+    { name: "Lino", age: 20 },
+    { name: "Ana", age: 25 },
+  ];
+  return (
+    <div>
+      <h1>{noRenderizado}</h1>
+      <p>
+        {friends[0].name} {friends[0].age}
+      </p>
+      <p>
+        {friends[1].name} {friends[1].age}
+      </p>
+    </div>
+  );
+}
+
+// ################# Export por defecto (el que correrá React) #################
+// export default AppProps;
+
+// Usar otros Componentes para probarlos
+
+// export default AppSimple; // Props
+
+// export default AppMultiple; // Múltiples Componentes
+
+export default AppNoRender; // No renderizar Objetos
