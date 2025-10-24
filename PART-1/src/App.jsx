5335868c4b9e3edfabc5a3308292d1c5644a4888
paulino-omnitec <paulino.salvador@omnitecsystems.com>
2025-10-25 00:59:53 +0200

    1 | Estados de componente

--- PART-1/src/App.jsx
+++ PART-1/src/App.jsx
@@ -1,100 +1,34 @@
-import "./App.css";
+// --------- INICIO COMPONENTE DE REACT ---------
 
-// ################# Componente App Simple #################
-export function AppSimple() {
-  console.log("Hello from AppSimple Component");
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log("APP Now : ", now, a + b);
-  return (
-    <>
-      <p>Bienvenido {now.toString()}</p>
-      <p>
-        {a} + {b} = {a + b}
-      </p>
-    </>
-  );
-}
-
-// ################# Componente App Multiple #################
-function HelloMultiple() {
+const Hello = (props) => {
+  // FuncioN auxiliar del componente
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
   return (
     <div>
-      <h2>Hello Component</h2>
+      <p>
+        Hello {props.name}, you are {props.age} years old{" "}
+      </p>
+      <p>So you were born in {bornYear()}</p>
     </div>
   );
-}
+};
 
-export function AppMultiple() {
-  console.log("Hello from AppMultiple Component");
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log("APP Now : ", now, a + b);
-  return (
-    <main>
-      <h1>Componentes Múltiples</h1>
-      <p>Texto introductorio que es solo inline.</p>
-      <HelloMultiple />
-      <HelloMultiple />
-    </main>
-  );
-}
+// --------- FIN COMPONENTE DE REACT ---------
 
-// ################# Props: pasar datos a componentes #################
-function HelloProps({ name, age, time }) {
-  console.log("props en Hello: ", { name, age, time });
+const App = () => {
+  const name = "Peter";
+  const age = 29;
+
   return (
     <div>
-      <h2>
-        Hello {name}, you are {age} years old
-      </h2>
+      <h1>Grettings</h1>
+      <Hello name={name} age={age} />
+      <Hello name="Maya" age={26 + 10} />
     </div>
   );
-}
+};
 
-export function AppProps() {
-  console.log("Hello from AppProps Component");
-  const now = new Date();
-  const name = "Lino";
-  const age = 20;
-  return (
-    <main>
-      <h1>Props</h1>
-      <HelloProps name={name} age={age} time={now} />
-      <HelloProps name="Antonio" age={20 - 5} />
-    </main>
-  );
-}
-
-// ################# No renderizar Objetos #################
-export function AppNoRender() {
-  const noRenderizado = "Objeto No Renderizado";
-  const friends = [
-    { name: "Lino", age: 20 },
-    { name: "Ana", age: 25 },
-  ];
-  return (
-    <div>
-      <h1>{noRenderizado}</h1>
-      <p>
-        {friends[0].name} {friends[0].age}
-      </p>
-      <p>
-        {friends[1].name} {friends[1].age}
-      </p>
-    </div>
-  );
-}
-
-// ################# Export por defecto (el que correrá React) #################
-// export default AppProps;
-
-// Usar otros Componentes para probarlos
-
-// export default AppSimple; // Props
-
-// export default AppMultiple; // Múltiples Componentes
-
-export default AppNoRender; // No renderizar Objetos
+export default App;
